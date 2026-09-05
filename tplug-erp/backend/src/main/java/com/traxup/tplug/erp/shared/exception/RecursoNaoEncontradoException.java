81fb47e1116fac05c3b5f22ae65d68e3108b7d18
DjonesErison <djones@cdssistemas.com.br>
2026-09-05 01:24:21 -0300

    feat(tplug-erp): adiciona excecao de recurso nao encontrado

---
+++ tplug-erp/backend/src/main/java/com/traxup/tplug/erp/shared/exception/RecursoNaoEncontradoException.java
@@ -0,0 +1,8 @@
+package com.traxup.tplug.erp.shared.exception;
+
+public class RecursoNaoEncontradoException extends RuntimeException {
+
+    public RecursoNaoEncontradoException(String mensagem) {
+        super(mensagem);
+    }
+}

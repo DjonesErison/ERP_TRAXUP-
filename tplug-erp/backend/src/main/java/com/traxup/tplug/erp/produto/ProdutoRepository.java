98e8eeccffdde5a75458b119033faac82ac9419b
DjonesErison <djones@cdssistemas.com.br>
2026-09-08 18:19:44 -0300

    feat(inventario): permite localizar produto por codigo de barras

--- tplug-erp/backend/src/main/java/com/traxup/tplug/erp/produto/ProdutoRepository.java
+++ tplug-erp/backend/src/main/java/com/traxup/tplug/erp/produto/ProdutoRepository.java
@@ -12,5 +12,7 @@ public interface ProdutoRepository extends JpaRepository<Produto, UUID> {
 
     Optional<Produto> findByIdAndTenantId(UUID id, UUID tenantId);
 
+    Optional<Produto> findFirstByTenantIdAndCodigoBarraAndAtivoTrue(UUID tenantId, String codigoBarra);
+
     boolean existsByTenantIdAndCodigoIgnoreCase(UUID tenantId, String codigo);
 }

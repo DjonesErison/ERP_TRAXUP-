9b545add0d2065ea3bb138d458dd63e75bd456a8
DjonesErison <djones@cdssistemas.com.br>
2026-09-08 08:31:53 -0300

    feat(combo): permite validar produto configurado como combo

--- tplug-erp/backend/src/main/java/com/traxup/tplug/erp/produto/combo/ProdutoComboGrupoRepository.java
+++ tplug-erp/backend/src/main/java/com/traxup/tplug/erp/produto/combo/ProdutoComboGrupoRepository.java
@@ -9,5 +9,6 @@ import java.util.UUID;
 public interface ProdutoComboGrupoRepository extends JpaRepository<ProdutoComboGrupo, UUID> {
     List<ProdutoComboGrupo> findAllByTenantIdAndComboProdutoIdOrderByNomeAsc(UUID tenantId, UUID comboProdutoId);
     Optional<ProdutoComboGrupo> findByIdAndTenantIdAndComboProdutoId(UUID id, UUID tenantId, UUID comboProdutoId);
+    boolean existsByTenantIdAndComboProdutoId(UUID tenantId, UUID comboProdutoId);
     boolean existsByTenantIdAndComboProdutoIdAndNomeIgnoreCase(UUID tenantId, UUID comboProdutoId, String nome);
 }

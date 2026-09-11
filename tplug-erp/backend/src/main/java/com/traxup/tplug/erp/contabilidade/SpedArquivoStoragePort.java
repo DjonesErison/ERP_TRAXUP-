d33cd9e929ce4fe0efa582ef39fe423dbe6a1c57
DjonesErison <djones@cdssistemas.com.br>
2026-09-10 23:06:14 -0300

    feat(contabilidade): permitir leitura do storage SPED

--- tplug-erp/backend/src/main/java/com/traxup/tplug/erp/contabilidade/SpedArquivoStoragePort.java
+++ tplug-erp/backend/src/main/java/com/traxup/tplug/erp/contabilidade/SpedArquivoStoragePort.java
@@ -2,4 +2,5 @@ package com.traxup.tplug.erp.contabilidade;
 
 public interface SpedArquivoStoragePort {
     void armazenar(String chave, byte[] conteudo, String hashSha256);
+    byte[] baixar(String chave);
 }
